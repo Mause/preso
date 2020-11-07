e3cf385d317a87ad774d2d67884340d4e1d26541
Elliana <me@mause.me>
2020-11-07 13:04:51 +0800

    Remove version demo

--- src/slides/Slide2.tsx
+++ src/slides/Slide2.tsx
@@ -1,32 +1,10 @@
 import * as React from 'react';
 import CodePage from '../CodePage';
-import KnownMount from '../KnownMount';
 import { strip } from '../strip';
 
-declare function require(path: string): string;
-const version_url = require('../version');
-
-interface Slide2State {
-  version: string | null;
-}
-interface Slide2Props {};
-
-class Slide2 extends KnownMount<Slide2Props> {
-  state: Slide2State;
-
-  constructor(props: Slide2Props) {
-    super(props);
-    this.state = { version: null };
-    this.initialise();
-  }
-  async initialise(): Promise<any> {
-    const response = await fetch(version_url);
-    let version = await response.text();
-    this.setState({ version: version.trim() });
-  }
+class Slide2 extends React.Component {
   render() {
     const codes = [
-      `"${this.state.version}"`,
       strip`
         from datetime import date
         date.today().isoformat()`,
