8850386eb55d5af85f1d0fce4b1754ebc61e4e5c
Elliana <me@mause.me>
2021-09-27 16:56:45 +0800

    Add a fifth slide

---
+++ src/slides/Slide5.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CodePage from "../CodePage";
+import { strip } from "../strip";
+
+export default function Slide5() {
+    return <CodePage codes={[
+        strip`
+        from cowsay import cow
+        from io import StringIO
+
+        sys.stdout = StringIO()
+        cow('hello')
+        sys.stdout.getvalue()
+        `
+    ]} />
+}
